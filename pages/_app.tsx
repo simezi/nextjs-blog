e12bd9fe0d4d3ba35c850e56d858f117becb1db5
simezi <matsubaray@binc.jp>
2020-11-06 19:33:55 +0900

    途中まで

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from 'next/app'
+import React from 'react'
+import '../styles/global.css'
+
+
+export default function App({Component, pageProps}: AppProps) {
+    return <Component {...pageProps}/>
+}
